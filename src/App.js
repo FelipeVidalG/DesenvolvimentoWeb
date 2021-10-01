cabed87a24064327c360eb6aea7cc1c98e6fabd6
felipevgevaerd@gmail.com <you@example.com>
2021-09-30 22:28:00 -0300

    adicionando app.js e app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import firebase from './config/Firebase';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = {
+        usuario: []
+      }
+
+      this.gravar = this.gravar.bind(this);
+      this.listar = this.listar.bind(this);
+    }
+
+     gravar(){
+        firebase.collection('usuario').add({
+        nome: 'Felipe',
+        sobrenome: 'Vidal'
+      }).then(()=>{
+        console.log("Gravou!");
+      }).catch((erro)=>{
+        console.log("erro: " + erro);
+      });
+    }
+
+    listar(){
+      firebase.collection('usuario').get()
+      .then((snapshot)=>{
+          let lista = [];
+          snapshot.forEach((item)=>{
+            lista.push({
+              id: item.id,
+              nome: item.data().nome,
+              sobrenome: item.data().sobrenome
+            });
+          });
+          console.log(lista);
+          this.setState({usuario: lista});
+      });
+    }
+
+
+    componentDidMount(){
+      firebase.collection('usuario').onSnapshot((snapshot)=>{
+          let lista = [];
+          snapshot.forEach((item)=>{
+            lista.push({
+              id: item.id,
+              nome: item.data().nome,
+              sobrenome: item.data().sobrenome
+            });
+          });
+          console.log(lista);
+          this.setState({usuario: lista});
+      });
+    }
+
+    render(){
+      return(
+        <div>
+
+          <h1>Firestore</h1>
+
+          <button onClick={this.gravar}> Gravar </button>
+          <br/><br/>
+          <button onClick={this.listar}> Listar </button>
+          <br/><br/>
+
+          {
+            this.state.usuario.map((item)=>{
+
+              return (
+                <div>
+                    {item.nome} <br/>
+                </div>
+              )
+
+            })
+          }
+
+        </div>
+      )
+    }
+
 }
+
+
 
 export default App;
