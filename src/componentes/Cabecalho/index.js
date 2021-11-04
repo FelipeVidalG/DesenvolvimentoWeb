19cf2a2860c79917dcfcd1b2e1ec3ec7dffa92a0
Guilherme.Ribeiro <ribeiroguilhermedev@gmail.com>
2021-11-03 21:00:38 -0300

    correção

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,67 +1,66 @@
-import react from "react";
+import React from "react";
 import 'bulma/css/bulma.min.css'
 import './index.css'
-import { Logo } from "../Logo";
 import logoCabecalho from "../../imagens/logoCabecalho.png"
 
 
 function Cabecalho() {
-  return (
-    <div>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <button classNames="navbar-item" href="#">
-            <img src={logoCabecalho} className="logo" alt="logo" />
-          </button>
+    return(
+        <div>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#">
+    <img src={logoCabecalho} className="logo" alt="logo" />
+    </a>
 
-          <button class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </button>
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Tela inicial
+      </a>
+
+      <a class="navbar-item">
+        Perfil
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Mais
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Convites
+          </a>
+          <a class="navbar-item">
+            Avaliação
+          </a>
+          <a class="navbar-item">
+            Entre em contato
+          </a>
+          <hr class="navbar-divider"/>
+          <a class="navbar-item">
+            Reporte um problema
+          </a>
         </div>
-
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div className="navbar-start">
-            <button className="navbar-item">
-              Tela inicial
-            </button>
-
-            <button className="navbar-item">
-              Perfil
-            </button>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-              <button className="navbar-link">
-                Mais
-              </button>
-
-              <div classNameass="navbar-dropdown">
-                <button className="navbar-item">
-                  Convites
-                </button>
-                <button className="navbar-item">
-                  Avaliação
-                </button>
-                <button className="navbar-item">
-                  Entre em contato
-                </button>
-                <hr className="navbar-divider" />
-                <button className="navbar-item">
-                  Reporte um problema
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-            </div>
-          </div>
-        </div>
-      </nav>
+      </div>
     </div>
-  )
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+      </div>
+    </div>
+  </div>
+</nav>
+        </div>
+    )
 }
 
 export default Cabecalho;
